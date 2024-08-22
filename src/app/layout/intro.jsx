bf2bab143e778310a82964c128e545792a4b2bfa
marrodri <laza.mar64@gmail.com>
2024-08-22 14:25:52 -0700

    intro update

--- src/app/layout/intro.jsx
+++ src/app/layout/intro.jsx
@@ -1,12 +1,13 @@
 export default function Intro() {
   ///TODO: build an intro object.
   return (
-    <section id="intro" className="py-20 w-full sm:bg-gray-400  sm:py-50">
-      <div className="text-left bg-green-800 h-40 px-5 sm:px-10">
+    <section id="intro" className="w-full sm:bg-gray-400  sm:py-50">
+      <div className="text-left bg-green-800 py-20 h-full px-10 sm:px-10">
         <h1 className="font-bold">Marthel Rodriguez</h1>
         <h3> Software Engineer and Tutor</h3>
         <br />
-        <p>Every moment at this current time</p>
+        <p>Every moment ...</p>
+        <p>Even in this current time ...</p>
         <p>I'm Always processing ... </p>
         <ul class="actions">
           <li>
