fae80d8d05c882188fa2c7245457c2351831491e
marrodri <laza.mar64@gmail.com>
2024-08-22 23:11:13 -0700

    footer padding

--- src/app/layout/footer.jsx
+++ src/app/layout/footer.jsx
@@ -1,11 +1,13 @@
 export default function Footer() {
   /// TODO: build a footer.
-  return <footer>
-    <div>
-        <ul>
-            <li>Marthel Rodriguez. All rights reserved.</li>
-            <li>This website was built with: ReactJS, TailwindCSS and NextJS</li>
+  return (
+    <footer>
+      <div className="py-10 mx-5">
+        <ul className="text-sm flex flex-col space-y-2">
+          <li>&copy; Marthel Rodriguez. All rights reserved.</li>
+          <li>This website was built with: ReactJS, TailwindCSS and NextJS</li>
         </ul>
-    </div>
-    </footer>;
+      </div>
+    </footer>
+  );
 }
